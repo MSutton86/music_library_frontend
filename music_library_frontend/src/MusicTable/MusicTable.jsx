c33e4648e4b13d816ded0b551964663a76224a3f
Mark S <stcoronawraps@gmail.com>
2023-02-06 15:53:16 -0700

    Added music Table + Added searchBar + outlining get req

---
+++ music_library_frontend/src/MusicTable/MusicTable.jsx
@@ -0,0 +1,29 @@
+const MusicTable = (props) => {
+  return (
+    <section id="music">
+      <h1>Music Table</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Album</th>
+            <th>Artis</th>
+            <th>Release Date</th>
+            <th>Genre</th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+      </table>
+    </section>
+  );
+};
+
+export default MusicTable;
