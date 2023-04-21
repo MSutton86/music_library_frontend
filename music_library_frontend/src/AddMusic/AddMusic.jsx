b4e7038a95fe1deeda462371714047bfbf0c588c
Mark S <stcoronawraps@gmail.com>
2023-04-21 12:52:13 -0700

    Task Add form to AddMusic completed. still needs function to handle submit

--- music_library_frontend/src/AddMusic/AddMusic.jsx
+++ music_library_frontend/src/AddMusic/AddMusic.jsx
@@ -9,7 +9,33 @@ const AddMusic = ({music}) => {
     return ( 
         <form onSubmit={handleSubmit}>
             <div>
-                
+                <h2>Add New Music</h2>
+                <label>Title</label>
+                <input
+                type='text'
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
+                />
+                <label>Artist</label>
+                <input
+                type='text'
+                value={artist}
+                onChange={(event) => setArtist(event.target.value)}
+                />
+                <label>Album</label>
+                <input
+                type='text'
+                value={album}
+                onChange={(event) => setAlbum(event.target.value)}/>
+                <label>Realease Date</label>
+                <input
+                type='text'
+                value={release_date}
+                onChange={(event) => setReleaseDate(event.target.value)}/>
+                <label>Genre</label>
+                <input
+                type='text'
+                value={(event) => setGenre(event.target.value)}/>
             </div>
             
         </form>
